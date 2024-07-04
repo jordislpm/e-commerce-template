f31fd20aadb915eb8fd378b7ed6b5a57d5935b59
jordislpm <jordislpm@gmail.com>
2024-07-03 22:07:38 -0400

    feat: start work with shopping cart

--- src/components/Layout/Header/Nav/index.tsx
+++ src/components/Layout/Header/Nav/index.tsx
@@ -16,8 +16,8 @@ function Nav() {
             route: ""
         }
     );
-    const {projectRoutes}=useProjectRoutes();
-        
+    const { projectRoutes } = useProjectRoutes();
+
     const toggleSubMenuState = () => {
         setSubMenu(prevState => ({
             ...prevState,
@@ -25,9 +25,9 @@ function Nav() {
         }));
     };
 
-    
+
     const navListClick = (actualRoute: RoutesListType) => {
-    
+
         setSubMenu({
             state: true,
             subRoute: actualRoute.subRoutes,
@@ -44,7 +44,7 @@ function Nav() {
             ...prevState,
             menu: [],
             state: false
-          }));
+        }));
     };
 
     return (
@@ -60,60 +60,60 @@ function Nav() {
             >
                 <IoCloseSharp onClick={closeModal} size={30} />
                 <div className={styles.navMenu}>
-     {!subMenu.state && <div className={styles.list}>
-                {projectRoutes.map((route, index) => (
-                    !route.subRoutes ? (
-                        <Link
-                        onClick={toggleModal}
-                            key={route.name}
-                            href={route.route}
-                            className={`${styles.link} ${index > 0 ? styles.link_middle : ""}`}
-                        >
-                            {route.name}
-                        </Link>
-                    ) : (
-                        <div
-                            key={route.name}
-                            onClick={() => navListClick(route)}
-                            className={`${styles.link} ${index > 0 ? styles.link_middle : ""}`}
-                        >
-                            {route.name}
-                            <IoIosArrowForward />
+                    {!subMenu.state && <div className={styles.list}>
+                        {projectRoutes.map((route, index) => (
+                            !route.subRoutes ? (
+                                <Link
+                                    onClick={toggleModal}
+                                    key={route.name}
+                                    href={route.route}
+                                    className={`${styles.link} ${index > 0 ? styles.link_middle : ""}`}
+                                >
+                                    {route.name}
+                                </Link>
+                            ) : (
+                                <div
+                                    key={route.name}
+                                    onClick={() => navListClick(route)}
+                                    className={`${styles.link} ${index > 0 ? styles.link_middle : ""}`}
+                                >
+                                    {route.name}
+                                    <IoIosArrowForward />
+                                </div>
+                            )
+                        ))}
+                    </div>}
+                    {subMenu.state &&
+                        <div className={`${styles.sub_list} ${subMenu ? styles.sub_list__active : ""}`}>
+                            <div
+                                className={styles.close_submenu}
+                                onClick={toggleSubMenuState}>
+                                <IoIosArrowBack />
+                                {subMenu.route}
+                            </div>
+                            {subMenu && subMenu?.subRoute?.map((route, index) => (
+
+                                <Link
+                                    onClick={toggleModal}
+                                    key={route.name}
+                                    href={`${route.subRoutes ? "" : route.route}`}
+                                    className={`${styles.link} ${styles.link_middle}`}
+                                >
+                                    {route.name}
+                                    {route.subRoutes &&
+                                        <IoIosArrowForward />}
+                                </Link>
+                            ))}
                         </div>
-                    )
-                ))}
-            </div>}
-            {subMenu.state &&
-                <div className={`${styles.sub_list} ${subMenu ? styles.sub_list__active : ""}`}>
-                    <div
-                    className={styles.close_submenu}
-                     onClick={toggleSubMenuState}>
-                        <IoIosArrowBack />
-                        {subMenu.route}
-                    </div>
-                    {subMenu && subMenu?.subRoute?.map((route, index) => (
+                    }
 
-                        <Link
-                        onClick={toggleModal}
-                            key={route.name}
-                            href={`${route.subRoutes ? "" : route.route}`}
-                            className={`${styles.link} ${styles.link_middle}`}
-                        >
-                            {route.name}
-                            {route.subRoutes &&
-                                <IoIosArrowForward />}
-                        </Link>
-                    ))}
+                    <Link
+                        href="/login"
+                        className={styles.link}
+                    >
+                        INICIAR SESION
+                    </Link>
                 </div>
-            }
-
-      <Link
-        href="/login"
-        className={styles.link}
-      >
-        INICIAR SESION
-      </Link>
-    </div>
             </div>
         </nav>
     )
--- src/components/Layout/Header/ShoppingCart/index.tsx
+++ src/components/Layout/Header/ShoppingCart/index.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import { RiShoppingCartLine } from "react-icons/ri";
+import { IoCloseSharp } from "react-icons/io5";
+import styles from "./ShoppingCart.module.css"
 
 function ShoppingCart() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
-      <RiShoppingCartLine size={25}/>
+      <div
+        className={`${styles.overlay} ${isOpen ? styles.overlayActive : ''}`}
+        onClick={toggleModal}>
+      </div>
+      <div className={`${styles.cart} ${isOpen ? styles.cartActive : styles.cartDesactive}`}>
+        <div className={styles.header}>
+          CARRITO
+          <IoCloseSharp onClick={toggleModal} size={25}  />
+        </div>
+        <div className={styles.body}> 
+            <div className={styles.vacio}>
+              EL CARRITO ESTA VACIO
+            </div>
+        </div>
+      </div>
+
+      <RiShoppingCartLine size={25} onClick={toggleModal} />
     </div>
   )
 }
