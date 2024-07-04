71f8db0cf996f165948464c06ee9d869dfd38f00
jordislpm <jordislpm@gmail.com>
2024-07-04 18:52:39 -0400

    feat: funciones basicas y ui de nav listo

--- src/components/Layout/Header/Search/index.tsx
+++ src/components/Layout/Header/Search/index.tsx
@@ -1,12 +1,51 @@
-import React from 'react'
-import { IoSearchOutline } from "react-icons/io5";
+"use client";
+import React, { useState, FormEvent } from "react";
+import { IoSearchOutline, IoCloseSharp } from "react-icons/io5";
+import styles from "./Search.module.css";
+
 
 function Search() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(`Buscando: ${searchTerm}`);
+  };
+
   return (
-    <div>
-        <IoSearchOutline size={25}/>
+    <div className={styles.main}>
+      <IoSearchOutline size={25} onClick={toggleModal} />
+      <div
+        className={`${styles.search} ${isOpen ? styles.searchActive : styles.searchDesactive
+          }`}
+      >
+         <form className={styles.form} onSubmit={handleSubmit}>
+      <button className={styles.button_search} type="submit">
+        <IoSearchOutline size={25} />
+      </button>
+      <input
+        className={styles.buscar}
+        type="text"
+        placeholder="Buscar..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+    </form>
+        <div className={styles.close}>
+        <IoCloseSharp onClick={toggleModal} size={30}/>
+        </div>
+      </div>
+      <div
+        className={`${styles.overlay} ${isOpen ? styles.overlayActive : ""}`}
+        onClick={toggleModal}
+      ></div>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
