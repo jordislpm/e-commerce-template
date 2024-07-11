fa73e3cf1fe5de1fd8d2c9723fa68e2a65c4933a
jordislpm <jordislpm@gmail.com>
2024-07-11 15:02:12 -0400

    feat: improve header styles and created modal for product details

--- src/components/Share/ProductCard/index.tsx
+++ src/components/Share/ProductCard/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import styles from "./ProductCard.module.css"
 import Image from 'next/image'
 import { Product } from "helebba-sdk"
@@ -14,11 +15,30 @@ interface ProductCardProps {
 
 function ProductCard({ product }: ProductCardProps) {
 
+  const [showDetails, setShowDetails] = useState<boolean>(false);
 
-  const { images, name , price} = product;
+  const toggleModal = () => {
+    console.log("click details")
+    setShowDetails(!showDetails);
+  }
+
+
+  const { images, name, price } = product;
   console.log(images[0]);
   return (
     <div className={styles.card}>
+      {/* details modal start */}
+      <div
+        className={`${styles.overlay} ${showDetails ? styles.overlayActive : ''}`}
+        onClick={toggleModal}>
+      </div>
+      <div className={`${styles.show_details} ${showDetails ? styles.show_details__active : styles.show_details__desactive}`}>
+          PRODUCT DETAILS
+
+      </div>
+      {/* details modal end */}
+
+
       <div className={styles.image_container}>
         <Image
           className={styles.image}
@@ -27,13 +47,15 @@ function ProductCard({ product }: ProductCardProps) {
           layout="responsive"
           src={images[0]}
           alt={name} />
-          <button className={styles.button_details}>
+        <button
+          onClick={toggleModal}
+          className={styles.button_details}>
           <FaPlus />
-          </button>
+        </button>
       </div>
       <div className={styles.info_container}>
-          <h3 className={styles.name}>{name}</h3>
-          <p className={styles.price}>Desde $/ {formatPrice(price)}</p>
+        <h3 className={styles.name}>{name}</h3>
+        <p className={styles.price}>Desde $/ {formatPrice(price)}</p>
       </div>
     </div>
   )
