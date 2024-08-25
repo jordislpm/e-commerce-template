e730a202ca30064686a06a408db30af1cc10ffd8
jordislpm <jordislpm@gmail.com>
2024-08-25 15:13:46 -0400

    fix: error for compiling

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,5 @@
 "use client"
 import Catalog from '@/components/Share/Catalog'
-import ProductsSection from '@/components/Share/ProductsSection'
-import useSearchStore from '@/hooks/global-state/useSearchGlobal'
 import SearchPrincipal from '@/sections/Search/SearchPrincipal'
 import React from 'react'
 
